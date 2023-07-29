308928d8e3320acb0407ec90d7a152c019a7ac42
ilya <sajfulin2015@gmail.com>
2023-07-29 20:30:27 +0300

    JS-INITIAL

---
+++ координаты/map2.js
@@ -0,0 +1,7 @@
+let x1 = 10, y1 = 5;
+let x2 = 2, y2 = 3;
+
+let area = Math.abs( (x2 - x1) * (y2 - y1) );
+
+console.log( "Площадь:", area );
+alert( "Площадь: " + area );
---
+++ округление/практ3.js
@@ -0,0 +1,16 @@
+let a = 13.890123;
+let b =  2.891564;
+let n = 3;
+
+let num1 = Math.round(a % 1 * Math.pow(10, n));
+let num2 = Math.round(b % 1 * Math.pow(10, n));
+
+console.log(num1, num2);
+
+console.log('Исходные числа равны', a === b);
+console.log('Оба числа равны', num1 === num2);
+console.log('Первое число больше', num1 > num2);
+console.log('Первое число меньше', num1 < num2);
+console.log('Первое число не равно второму', num1 != num2);
+console.log('Первое число больше либо равно первому', num1 >= num2);
+console.log('Первое число меньше либо равно первому', num1 <= num2);
